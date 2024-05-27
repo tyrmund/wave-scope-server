00dc5115cd55a18dc674b60aabe385b6967b700a
Paloma Rehecho <paloma_re@hotmail.com>
2024-05-27 09:49:22 +0100

    new categories in beach composition

--- models/Beach.model.js
+++ models/Beach.model.js
@@ -19,16 +19,12 @@ const beachSchema = new Schema(
       }
     },
     transportCoords: {
-<<<<<<< HEAD
-      type: [String]
-=======
       type: {
         type: String
       },
       coordinates: {
         type: [[Number]]
       }
->>>>>>> 74c92ce87698addb71a310280632ca76d2c44e2f
     },
     length: {
       type: Number,
@@ -36,7 +32,7 @@ const beachSchema = new Schema(
     },
     composition: {
       type: String,
-      enum: ["Sand", "Gravel", "Rock", "Shell"]
+      enum: ["Sand", "Gravel", "Rock", "Shell", "Sand and Rock", "Sand and Gravel", "Sand and Shell", "Gravel and Rock", "Gravel and Shell", "Rock and Shell"]
     },
     sectors: {
       type: Number,
